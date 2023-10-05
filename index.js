cc2d7ed7b76c1301e80f11406a95df41560d0246
moses <moses.kingi@moringaschool.com>
2023-10-05 12:30:37 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Kingi Jeremy';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+//solution two
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
